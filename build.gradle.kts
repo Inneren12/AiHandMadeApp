3fa1b0252ccd0b0dbe44101930db5292906fb93f
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-10-19 00:07:29 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
-// Root build script: фиксируем версии плагинов и загружаем их ОДИН РАЗ.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.20" apply false
+    id("com.android.application") version "8.12.3" apply false
+    id("com.android.library") version "8.12.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+}
+
+allprojects {
+    group = "com.AIHandMadeApp"
+    version = "1.0.0"
 }

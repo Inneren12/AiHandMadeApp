447f41be079ac0497e2b97ae0ebd6f99c214610c
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-10-18 19:47:43 -0600

    chore(build): move plugin management to settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,25 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "2.0.20"
+        id("org.jetbrains.kotlin.jvm") version "2.0.20"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "AiHandMadeApp"
 include(":app", ":core")
